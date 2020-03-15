6db318093baf05dde1f1ae310332188c260e5901
qiuyucc <china.cqy@outlook.com>
2020-03-15 15:35:36 +1100

    router for a signle application

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Menu from './Menu';
 import DishDetails from './DishdetailComponent';
+import Home from  './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -15,16 +17,20 @@ class Main extends Component {
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
-
   render() {
+      const HomePage  =()=>{
+          return(
+              <Home />
+          );
+      }
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetails dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Redirect to="/home" />
+          </Switch>
         <Footer />
       </div>
     );
