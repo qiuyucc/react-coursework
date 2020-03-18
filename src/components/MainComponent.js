e008675bfa70ee2b64a44fead6d97a17286bc224
qiuyucc <china.cqy@outlook.com>
2020-03-18 14:17:24 +1100

    coursework 2 fina

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Menu from './Menu';
 import DishDetails from './DishdetailComponent';
 import Home from  './HomeComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
@@ -9,6 +10,7 @@ import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
+
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -47,6 +49,7 @@ class Main extends Component {
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component ={DishWithId}/>
               <Route exact path='/contactus' component={ Contact }/>
+              <Route exact path='/aboutus' component ={()=> <About leaders={this.state.leaders}/>} />
               <Redirect to="/home" />
           </Switch>
         <Footer />
